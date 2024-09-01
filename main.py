c7f831200777c6ca2a506c8319c77f42d46d4ff1
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-09-02 01:25:04 +0900

    fix japanese

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ def main():
     # Title
     st.sidebar.title("JobMatch")
     st.sidebar.write("""Fill in your resume info to see which Money Forward job matches you best.""")
-    st.sidebar.write("""履歴書を入力して、ピッタリの求人を探そう！""")
+    st.sidebar.write("""レジュメを入力して、ピッタリの求人を探そう！""")
 
     # Input fields
     # Select input method: Copy and paste text or upload a file
-    resume_method = st.sidebar.radio("""Resume input レジュメ入力""", ("File", "Text"), horizontal = True)
+    resume_method = st.sidebar.radio("""Resume input (レジュメ入力)""", ("File", "Text"), horizontal = True)
 
     # Input: Text
     if resume_method == "Text":
-        resume_text = st.sidebar.text_area("Text Input テキスト入力", height=200)
+        resume_text = st.sidebar.text_area("Text Input (テキスト入力)", height=200)
     # Input: File Upload
     elif resume_method == "File":
-        resume_file = st.sidebar.file_uploader("File Upload ファイル・アップロード", type=["pdf", "docx", "txt"])
+        resume_file = st.sidebar.file_uploader("File Upload (アップロード)", type=["pdf", "docx", "txt"])
         if resume_file is not None:
             resume_text = read_resume(resume_file)
 
-    match = st.sidebar.radio("Match method マッチ方法", ("ChatGPT", "BERT"))
+    match = st.sidebar.radio("Match method (マッチ方法)", ("ChatGPT", "BERT"))
 
     # Submit button
     if st.sidebar.button("Submit"):
