636905020388cf2e6933a9fd16a2da32a8dd5c63
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-09-01 18:22:54 +0900

    made find_jobs

--- src/functions.py
+++ src/functions.py
@@ -65,6 +65,67 @@ def match_percentage(resume_text, jd_text):
     return content
 
 
+def find_jobs(resume_text, jd_text):
+
+    # streamlit
+    client = OpenAI()
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system",
+             "content": "You are a tech recruiter screening resumes."
+            },
+            {
+                "role": "user",
+                "content": f"""
+                Resume: ```{resume_text}```
+                Job Descriptions: ```{jd_text}```
+
+                You will be given 1 resume and many job titles along with their descriptions.
+                Please analyze the resume, and find the 3 best matching jobs based on the resume.
+
+                After you've identified the 3 best matching jobs, please compare the resume to each job description and give an estimated match percentage for each job.
+
+                Please use the below grading rubric to determine the estimated qualification percentage (do NOT output the rubric in the final response):
+                Please penalize heavily for any missing mandatory qualifications.
+
+                # Qualifications:
+                [✅/❌] [Qualification 1]: [What you can tell from the resume]
+                [✅/❌] [Qualification 2]: [What you can tell from the resume]
+                etc.
+
+                # Nice-to-have:
+                [✅/❌] [Nice-to-have 1]: [What you can tell from the resume]
+                [✅/❌] [Nice-to-have 2]: [What you can tell from the resume]
+                etc.
+
+
+                FINAL RESPONSE OUTPUT FORMAT:
+                ```
+                ## Job 1: [Job Title 1]
+                Estimated match percentage: [percentage]
+
+                ## Job 2: [Job Title 2]
+                Estimated match percentage: [percentage]
+
+                ## Job 3: [Job Title 3]
+                Estimated match percentage: [percentage]
+                ```
+
+                """
+            }
+        ]
+    )
+
+    print(completion)
+    print(completion.choices[0].message)
+
+    content = completion.choices[0].message.content
+
+    return content
+
+
 def read_resume(file):
     '''
     PDFPlumber is a Python library that extracts text, tables, and images from PDF files.
