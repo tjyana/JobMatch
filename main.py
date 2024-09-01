fd1d989c0dcc9fc35c26ae6a0be15ec2ce4f5928
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-09-02 01:19:17 +0900

    add jp to ui

--- main.py
+++ main.py
@@ -1,96 +1,39 @@
 import streamlit as st
-from src.functions import match_percentage, read_resume
-from src.BERT_function import match_resume
+from src.functions import read_resume
 from main_BERT import submit_BERT
 from main_GPT import submit_GPT
-from dotenv import load_dotenv
-import re
-import os
-import time
-
-# load_dotenv()
-# api_key = os.getenv('OPENAI_API_KEY')
-
-# additional features:
-# job match mode: take resume input > compare to JD database and find best matches
-# pdf upload: upload resume and JD as pdfs
 
 
-########### original main function ############
 
 def main():
     # Title
     st.sidebar.title("JobMatch")
-    st.sidebar.write("""Fill in your resume info to see which Money Forward job matches you best.""")
+    st.sidebar.write("""Fill in your resume info to see which Money Forward job matches you best. \n
+                     履歴書を入力して、ピッタリの求人を探そう！
+                     """)
 
     # Input fields
     # Select input method: Copy and paste text or upload a file
-    resume_method = st.sidebar.radio("""Choose Resume input method:""", ("File", "Text"), horizontal = True)
+    resume_method = st.sidebar.radio("""Resume input レジュメ入力""", ("File", "Text"), horizontal = True)
 
     # Input: Text
     if resume_method == "Text":
-        resume_text = st.sidebar.text_area("Paste Resume text", height=200)
+        resume_text = st.sidebar.text_area("Text Input テキスト入力", height=200)
     # Input: File Upload
     elif resume_method == "File":
-        resume_file = st.sidebar.file_uploader("Upload Resume file", type=["pdf", "docx", "txt"])
+        resume_file = st.sidebar.file_uploader("File Upload ファイル・アップロード", type=["pdf", "docx", "txt"])
         if resume_file is not None:
             resume_text = read_resume(resume_file)
 
-    match = st.sidebar.radio("Match with ", ("ChatGPT", "BERT"))
-
+    match = st.sidebar.radio("Match method マッチ方法", ("ChatGPT", "BERT"))
 
     # Submit button
     if st.sidebar.button("Submit"):
-        # Process the inputs
-        # st.session_state.resume_text = resume_text
 
         if match == "ChatGPT":
             submit_GPT(resume_text)
         elif match == "BERT":
             submit_BERT(resume_text)
-
-        # process_inputs(resume_text)
-
-
-def process_inputs(resume_text):
-    with st.spinner("Finding jobs..."):
-        output = match_resume(resume_text)
-    with st.spinner("Assessing fit..."):
-        st.write("You might be a good fit for these jobs:")
-        results = match_percentage(resume_text, output)
-        st.write(" ", results)
-
-
-
-
-
-
-
-
-
-# def process_inputs(resume_text, output):
-#     st.write("You might be a good fit for these jobs:")
-#     results = match_percentage(resume_text, output)
-#     st.write(" ", results)
-
-    # Function to display the final output:
-    # Top 3 job matches plus estimated qualification percentage
-
-    # for i, job in enumerate(output, 1):
-    #     st.write(f"{i}: {job['Title']}")
-    #     comparison = compare_resume(resume_text, job['Description'])
-    #     pattern = r'Estimated qualification percentage: \d+%'
-    #     match = re.search(pattern, comparison)
-    #     if match:
-    #         st.write(match.group(0))
-    #     else:
-    #         st.write("")
-    #     st.write("")
-
-
-
-
-
 
 
 
