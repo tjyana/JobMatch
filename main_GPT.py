c3134868c43df1d22f30913fb217bdbeb8dd4fec
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-09-03 23:54:54 +0900

    added the extra line for output

--- main_GPT.py
+++ main_GPT.py
@@ -1,110 +1,13 @@
 import streamlit as st
-from src.functions import match_percentage, read_resume, find_jobs
-from src.BERT_function import match_resume
-from dotenv import load_dotenv
-import re
-import os
-import time
+from src.functions import find_jobs
 import pandas as pd
-
-# load_dotenv()
-# api_key = os.getenv('OPENAI_API_KEY')
-
-# additional features:
-# job match mode: take resume input > compare to JD database and find best matches
-# pdf upload: upload resume and JD as pdfs
-
-
-# trying to create all chatgpt version of the main function
-# pretty much taking out match_resume and doing everything within match_percentage
-
-def main():
-    # Title
-    st.sidebar.title("JobMatch")
-    st.sidebar.write("""Fill in your resume info to see which Money Forward job matches you best.""")
-
-    # Input fields
-    # Select input method: Copy and paste text or upload a file
-    resume_method = st.sidebar.radio("""Choose Resume input method:""", ("File", "Text"), horizontal = True)
-
-    # Input: Text
-    if resume_method == "Text":
-        resume_text = st.sidebar.text_area("Paste Resume text", height=200)
-    # Input: File Upload
-    elif resume_method == "File":
-        resume_file = st.sidebar.file_uploader("Upload Resume file", type=["pdf", "docx", "txt"])
-        if resume_file is not None:
-            resume_text = read_resume(resume_file)
-
-    # Submit button
-    if st.sidebar.button("Submit"):
-
-        # jobs = pd.read_csv('resume-data/jobs.csv')
-
-        # with st.spinner("Finding jobs..."):
-        #     st.write("You might be a good fit for these jobs:")
-        #     results = find_jobs(resume_text, jobs)
-        #     st.write(" ", results)
-
-
-
-        process_inputs(resume_text)
-
-
-def process_inputs(resume_text):
-    with st.spinner("Finding jobs..."):
-        jobs = pd.read_csv('resume-data/jobs.csv')
-
-        st.write("You might be a good fit for these jobs:")
-        results = find_jobs(resume_text, jobs)
-        st.write(" ", results)
 
 
 
 def submit_GPT(resume_text):
     with st.spinner("Finding jobs..."):
         jobs = pd.read_csv('resume-data/jobs.csv')
-        # csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../resume-data/jobs.csv'))
-        # jobs = pd.read_csv(csv_path)
-
-        st.write("You might be a good fit for these jobs:")
+        st.write("You might be a good fit for these jobs ")
+        st.write("この仕事があってるかも")
         results = find_jobs(resume_text, jobs)
         st.write(" ", results)
-
-
-
-        # # Assess fit and display results
-        # with st.spinner("Assessing fit..."):
-        #     process_inputs(resume_text, jobs)
-
-
-# def process_inputs(resume_text, output):
-#     jobs = pd.read_csv('resume-data/jobs.csv')
-
-#     st.write("You might be a good fit for these jobs:")
-#     results = find_jobs(resume_text, jobs)
-#     st.write(" ", results)
-
-    # Function to display the final output:
-    # Top 3 job matches plus estimated qualification percentage
-
-    # for i, job in enumerate(output, 1):
-    #     st.write(f"{i}: {job['Title']}")
-    #     comparison = compare_resume(resume_text, job['Description'])
-    #     pattern = r'Estimated qualification percentage: \d+%'
-    #     match = re.search(pattern, comparison)
-    #     if match:
-    #         st.write(match.group(0))
-    #     else:
-    #         st.write("")
-    #     st.write("")
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    main()
