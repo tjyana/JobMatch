6a9ff463de937c8bc9900f7ade0208b3d48d486d
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-09-07 23:15:28 +0900

    switch back to absolute file patch

--- main_BERT.py
+++ main_BERT.py
@@ -13,10 +13,10 @@ Use semantic similarity to match resume to job descriptions
 '''
 
 def get_df():
-    # csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../resume-data/jobs.csv'))
-    # df = pd.read_csv(csv_path)
-    df = pd.read_csv('resume-data/jobs.csv')
-    print('get_df df:', df.head())
+    csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../resume-data/jobs.csv'))
+    df = pd.read_csv(csv_path)
+    # df = pd.read_csv('resume-data/jobs.csv')
+    # print('get_df df:', df.head())
     return df
 
 
