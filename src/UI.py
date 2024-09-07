48f3ade08230ad0e70f88bd730d3487e2d5d21b8
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-09-07 23:57:48 +0900

    moved read_resume

--- src/UI.py
+++ src/UI.py
@@ -2,7 +2,7 @@ import streamlit as st
 from src.functions import read_resume
 
 def UI_resume_input():
-    # Input fields
+
     # Select input method: Copy and paste text or upload a file
     resume_method = st.sidebar.radio("""Resume input (レジュメ入力)""", ("File", "Text"), horizontal = True)
 
@@ -10,6 +10,7 @@ def UI_resume_input():
     if resume_method == "Text":
         resume_text = st.sidebar.text_area("Text Input (テキスト入力)", height=200)
         return resume_text
+
     # Input: File Upload
     elif resume_method == "File":
         resume_file = st.sidebar.file_uploader("File Upload (アップロード)", type=["pdf", "docx", "txt"])
