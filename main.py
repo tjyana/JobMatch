4ee59963eaf3afdcd34946480dbc2158c32e6bc4
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-09-07 23:14:50 +0900

    clean pu

--- main.py
+++ main.py
@@ -11,33 +11,12 @@ def main():
     st.sidebar.write("""Fill in your resume info to see which Money Forward job matches you best.""")
     st.sidebar.write("""レジュメを入力して、ピッタリの求人を探そう！""")
 
-    # # Input fields
-    # # Select input method: Copy and paste text or upload a file
-    # resume_method = st.sidebar.radio("""Resume input (レジュメ入力)""", ("File", "Text"), horizontal = True)
-
-    # # Input: Text
-    # if resume_method == "Text":
-    #     resume_text = st.sidebar.text_area("Text Input (テキスト入力)", height=200)
-    # # Input: File Upload
-    # elif resume_method == "File":
-    #     resume_file = st.sidebar.file_uploader("File Upload (アップロード)", type=["pdf", "docx", "txt"])
-    #     if resume_file:
-    #         resume_text = read_resume(resume_file)
-
     resume_text = UI_resume_input()
 
     match = st.sidebar.radio("Match method (マッチ方法)", ("ChatGPT", "BERT (MiniLM)", "BERT (Multilingual)", "BERT (cl-tohoku)", "BERT (sonoisa)"))
 
     # Submit button
     if st.sidebar.button("Submit"):
-
-        # if match == "ChatGPT":
-        #     output = submit_GPT(resume_text)
-        # elif match == "BERT (MiniLM)":
-        #     output = submit_BERT_minilm(resume_text)
-        # elif match == "BERT (Multilingual)":
-        #     output = submit_BERT_multilingual(resume_text)
-
         if match == "ChatGPT":
             output = submit_GPT(resume_text)
         else:
