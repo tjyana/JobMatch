dbb896864f54250f1d1a4b987423cf74360d0e4d
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-09-07 18:26:58 +0900

    trying again

--- main_BERT.py
+++ main_BERT.py
@@ -35,7 +35,7 @@ def get_df():
     # csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../resume-data/jobs.csv'))
     # df = pd.read_csv(csv_path)
     df = pd.read_csv('resume-data/jobs.csv')
-
+    print('get_df df:', df.head())
     return df
 
 # def get_model(match):
